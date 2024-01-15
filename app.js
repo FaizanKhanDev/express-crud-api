124eee6c1bf5a4d9fc9fa55c9dfc58fa91ae05ea
FaizanKhanDev <faizan480khan@gmail.com>
2024-01-15 00:10:31 -0800

    ADDED: Corse has been Step

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import connectDB  from "./db/connectiondb.js";
+import cors from "cors"; // Import the cors package
+
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017";
 import web from "./routes/web.js";
 connectDB(DATABASE_URL);
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 5000;
+// Use cors middleware to enable CORS
+app.use(cors());
 app.use('/student', web);
 app.listen(port, () => {
     console.log(`Listening on port http://localhost:${port}`);
