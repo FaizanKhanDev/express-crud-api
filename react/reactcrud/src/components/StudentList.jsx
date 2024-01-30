d0138624ced6841cac34bfd417bb5c1950461516
FaizanKhanDev <faizan480khan@gmail.com>
2024-01-29 23:20:04 -0800

    FIXED create stundet

--- react/reactcrud/src/components/StudentList.jsx
+++ react/reactcrud/src/components/StudentList.jsx
@@ -2,8 +2,10 @@ import { useGetAllStudentsListQuery } from "../services";
 
 function StudentList() {
     const { data, error, isLoading } = useGetAllStudentsListQuery();
-
-    return (
+    const editStudent = (student) => {
+        console.log(student);
+    }
+    return (    
         <div>
             <ul>
                 {isLoading && <p>Loading...</p>}
@@ -14,6 +16,9 @@ function StudentList() {
                             <h2>Name: {student.name}</h2>
                             <li>Age: {student.age}</li>
                             <li>Fees: ${parseFloat(student.fees.$numberDecimal).toFixed(2)}</li>
+                            <button onClick={() => { editStudent(student) } }>Edit</button>
+                            <button onClick={() => { alert("Are you sure you want to delete?"); }}>Delete</button>
+                            <hr />
                             <br />
                         </div>
                     ))}
