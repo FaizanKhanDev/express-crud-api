4f8b66ca92e16cdd1207a8c28d6f147bf3926439
FaizanKhanDev <faizan480khan@gmail.com>
2024-01-29 22:33:58 -0800

    created react A[[

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -3,7 +3,7 @@ import studentModel from "../models/Student.js";
 
 
 class StudentController {
-    static createDoc = async (req, res)  =>{
+    static createDoc = async (req, res) => {
         try {
             const { name, age, fees } = req.body;
             const result = await studentModel({
@@ -13,48 +13,48 @@ class StudentController {
             });
             await result.save();
             res.status(201).send(result);
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
     }
 
-    static getAllDocs = async (req,res) => {
+    static getAllDocs = async (req, res) => {
         try {
-            const result =  await studentModel.find();
-                res.send(result )
-        }catch(err){
+            const result = await studentModel.find();
+            res.send(result)
+        } catch (err) {
             console.log(err);
         }
-    }   
-
-    static getDocById = async (req,res) => {
-            try {
-                const result =  await studentModel.findById(req.params.id);
-                    res.send(result)
-            } catch(err){
-                console.log(err);
-            }
     }
-    static updateDocById = async (req,res) => {
+
+    static getDocById = async (req, res) => {
+        try {
+            const result = await studentModel.findById(req.params.id);
+            res.send(result)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    static updateDocById = async (req, res) => {
 
         try {
-            const findStudent = await studentModel.findByIdAndUpdate(req.params.id,  req.body);
+            const findStudent = await studentModel.findByIdAndUpdate(req.params.id, req.body);
             res.send(findStudent)
         } catch (error) {
             console.log(error);
-            
+
         }
     }
-    static deleteDocById = async (req,res) => {
+    static deleteDocById = async (req, res) => {
         try {
             const deleteStudent = await studentModel.findByIdAndDelete(req.params.id);
             res.send(deleteStudent)
         } catch (error) {
             console.log(error);
-            
+
         }
 
     }
 }
 
-export default  StudentController
+export default StudentController
